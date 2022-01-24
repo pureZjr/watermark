27b4644bc70c92441905c3e0ad408b0f92bea7d1
zhangjunrao <zhangjunrao@chicv.com>
2022-01-24 14:35:01 +0800

    up: 整体流程

--- index.js
+++ index.js
@@ -1,89 +1,128 @@
-const express = require('express')
-const path = require('path')
-const multer = require('multer')
-const child_process = require('child_process')
-const cors = require('cors')
-const { v4: uuidv4 } = require('uuid')
-const fs = require('fs')
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const child_process = require('child_process');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const JSZip = require('jszip');
 
-const app = express()
+const app = express();
 
 //跨域请求cors
 app.use(
   cors({
     origin: '*',
     credentials: true,
-  })
-)
-app.use(express.static('template'))
+  }),
+);
+app.use(express.static('template'));
 
 // 自定义 multer 的 diskStorage 的存储目录与文件名
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'imgs')
+    cb(null, 'imgs');
   },
   filename: function (req, file, cb) {
-    cb(null, `${uuidv4()}_${file.fieldname}`)
+    cb(null, `${uuidv4()}_${file.fieldname}`);
   },
-})
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 // 页面渲染
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'view/upload.html'))
-})
+  res.sendFile(path.join(__dirname, 'view/upload.html'));
+});
 
 // 加水印
 app.post('/upload', upload.any(), async function (req, res) {
-  const { path: filePath, filename } = req.files[0]
-  const embedPath = `embed/${filename}`
-  await exec('embed', req.query, filePath, embedPath)
-  const fileData = fs.readFileSync(embedPath)
-  fs.unlinkSync(filePath)
-  fs.unlinkSync(embedPath)
-  res.send({
-    fileData,
-    fileName: `water_mark_${filename}`,
-  })
-})
+  const { path: filePath, filename } = req.files[0];
+  const embedPath = `embed/${filename}`;
+  const delFile = () => {
+    fs.unlinkSync(filePath);
+    fs.unlinkSync(embedPath);
+  };
+  try {
+    // 保存水印图片路径
+    const { stdout } = await exec('embed', req.query, filePath, embedPath);
+    // 水印形状
+    const watermarkSize = parseFloat(
+      stdout.split('Put down watermark size: ')[1],
+    );
+    const zip = new JSZip();
+    const folder = zip.folder('watermark');
+    folder.file(
+      `size_${uuidv4()}.txt`,
+      `你的水印形状是：${watermarkSize}；
+       你的水印密码是：${req.query.pwd}；
+      解密的时候需要填写此参数`,
+    );
+    folder.file(filename, fs.readFileSync(embedPath));
+    zip
+      .generateAsync({ type: 'base64' })
+      .then((content) => {
+        delFile();
+        res.status(200).send({ fileData: content, fileName: 'watermark.zip' });
+      })
+      .catch((err) => {
+        delFile();
+        console.log(err);
+        res.status(500).send({});
+      });
+  } catch (err) {
+    delFile();
+    console.log(err);
+    res.status(500).send({});
+  }
+});
 
 // 获取水印信息
 app.post('/get-info', upload.any(), async (req, res) => {
-  const { path: filePath, filename } = req.files[0]
-  const extractPath = `imgs/${filename}`
-  const info = await exec('extract', req.query, filePath, extractPath)
-  fs.unlinkSync(extractPath)
-  console.log(info, req.data)
-})
+  try {
+    const { path: filePath, filename } = req.files[0];
+    const extractPath = `imgs/${filename}`;
+    const { stdout } = await exec('extract', req.query, filePath, extractPath);
+    const txt = stdout.split('Extract succeed! watermark is:\n')[1];
+    const watermarkInfo = txt.substring(-1, txt.length - 2);
+    fs.unlinkSync(extractPath);
+    res.send({
+      watermarkInfo,
+    });
+  } catch {
+    fs.unlinkSync(extractPath);
+    res.send({
+      watermarkInfo: '获取信息失败',
+    });
+  }
+});
 
 // 执行python命令
 const exec = (type, params, inputFilePath, embedFile) => {
   return new Promise((resolve, reject) => {
-    let order = ''
+    let order = '';
     if (type === 'embed') {
-      order += 'blind_watermark --embed '
-      order += `--pwd ${params.pwd} `
-      order += `"${inputFilePath}" `
-      order += `${params.info} `
-      order += `"${embedFile}"`
+      order += 'blind_watermark --embed ';
+      order += `--pwd ${params.pwd} `;
+      order += `"${inputFilePath}" `;
+      order += `${params.info} `;
+      order += `"${embedFile}"`;
     } else {
-      order += 'blind_watermark --extract --wm_shape 111 '
-      order += `--pwd ${params.pwd} `
-      order += `"${inputFilePath}"`
+      order += `blind_watermark --extract --wm_shape ${params.size} `;
+      order += `--pwd ${params.pwd} `;
+      order += `"${inputFilePath}"`;
     }
-    console.log(order)
     child_process.exec(order, function (error, stdout, stderr) {
       if (error) {
-        console.log(error)
-        reject({})
+        console.log(error);
+        reject({});
       }
       resolve({
         stdout,
         stderr,
-      })
-    })
-  })
-}
+      });
+    });
+  });
+};
 
-app.listen(3002, '127.0.0.1', function () {})
+app.listen(3002, '127.0.0.1', function () {});
--- template/script.js
+++ template/script.js
@@ -1,51 +1,47 @@
 $('#file').on('change', (e) => {
-  const reads = new FileReader()
-  const f = $('#file')[0].files[0]
-  reads.readAsDataURL(f)
+  const reads = new FileReader();
+  const f = $('#file')[0].files[0];
+  reads.readAsDataURL(f);
   reads.onload = function () {
-    document.getElementById('preview').src = this.result
-    $('#preview').css('display', 'block')
-  }
-})
+    document.getElementById('preview').src = this.result;
+    $('#preview').css('display', 'block');
+  };
+});
 
 // 加水印
 $('.btns span')
   .eq(0)
   .on('click', () => {
     try {
-      const file = $('#file')[0].files[0]
-
-      const formData = new FormData()
-      formData.append(file.name, file)
-      const pwd = $('#pwd').val()
-      const info = $('#info').val()
+      const file = $('#file')[0].files[0];
+      console.log(file);
+      const formData = new FormData();
+      formData.append(file.name, file);
+      const pwd = $('#pwd').val();
+      const info = $('#info').val();
       if (!info) {
-        alert('请输入水印信息')
+        alert('请输入水印信息');
       }
       if (!pwd) {
-        alert('请输入水印密码')
+        alert('请输入水印密码');
       }
       const onProgress = (event) => {
         document.querySelector('.progress span').style.width =
-          event.percent + '%'
-      }
-      const onSuccess = ({ fileData, fileName }) => {
-        const view = new Uint8Array(new ArrayBuffer(fileData.data.length))
-        for (var i = 0; i < fileData.data.length; ++i) {
-          view[i] = fileData.data[i]
-        }
-        const blob = new Blob([view], {
-          type: 'image/jpeg,image/jpg,image/png',
-        })
-        const downloadElement = document.createElement('a')
-        const href = window.URL.createObjectURL(blob) //创建下载的链接
-        downloadElement.href = href
-        downloadElement.download = fileName //下载后文件名
-        document.body.appendChild(downloadElement)
-        downloadElement.click() //点击下载
-        document.body.removeChild(downloadElement) //下载完成移除元素
-        window.URL.revokeObjectURL(href) //释放掉blob对象
-      }
+          event.percent + '%';
+      };
+      const onSuccess = async ({ fileData, fileName, watermarkSize }) => {
+        const zip = new JSZip();
+        await zip.loadAsync(fileData, { base64: true });
+        const blob = await zip.generateAsync({ type: 'blob' });
+        const downloadElement = document.createElement('a');
+        const href = window.URL.createObjectURL(blob); //创建下载的链接
+        downloadElement.href = href;
+        downloadElement.download = fileName; //下载后文件名
+        document.body.appendChild(downloadElement);
+        downloadElement.click(); //点击下载
+        document.body.removeChild(downloadElement); //下载完成移除元素
+        window.URL.revokeObjectURL(href); //释放掉blob对象
+      };
       upload({
         formData,
         onProgress,
@@ -54,56 +50,65 @@ $('.btns span')
           info,
           pwd,
         },
-      })
+      });
     } catch {
-      alert('请重新上传图片')
+      alert('请重新上传图片');
     }
-  })
+  });
 
 // 提取信息
 $('.btns span')
   .eq(1)
   .on('click', () => {
-    const file = $('#file')[0].files[0]
-    const formData = new FormData()
-    formData.append(file.name, file)
-    const pwd = $('#pwd').val()
+    const file = $('#file')[0].files[0];
+    const formData = new FormData();
+    formData.append(file.name, file);
+    const pwd = $('#pwd').val();
     if (!pwd) {
-      alert('请输入水印密码')
+      alert('请输入水印密码');
     }
-    const onSuccess = () => {}
+    const size = $('#size').val();
+    if (!size) {
+      alert('请输入水印形状');
+    }
+    const onSuccess = (pwd) => {
+      console.log(pwd);
+    };
     getInfo({
       formData,
       onSuccess,
       params: {
         pwd,
+        size,
       },
-    })
-  })
+    });
+  });
 
 document.getElementById('file').onchange = function () {
   const fileList = this.files,
-    formData = new FormData()
+    formData = new FormData();
   Array.prototype.forEach.call(fileList, function (file) {
-    formData.append(file.name, file)
-  })
+    formData.append(file.name, file);
+  });
   // 当上传的数据为 file 类型时，请求的格式类型自动会变为 multipart/form-data, 如果头部格式有特定需求，在我的 http 函数中传入 headers<Object> 即可，大家可自己查看，我这里没有什么特殊处理所以就不传了
-}
+};
 
 // 切换类型
 $('.check-wrap').on('click', (e) => {
-  const $ev = $(e.currentTarget)
-  const name = $ev.data('name')
-  $ev.find('.radio').text('✅')
-  $ev.siblings().find('.radio').text('')
+  const $ev = $(e.currentTarget);
+  const name = $ev.data('name');
+  $ev.find('.radio').text('✅');
+  $ev.siblings().find('.radio').text('');
   if (name === 'extract') {
-    $('.btns').removeClass('embed').addClass(name)
-    $('.input-wrap').eq(0).css('display', 'none')
+    $('.btns').removeClass('embed').addClass(name);
+    $('.input-wrap').eq(0).css('display', 'none');
+    $('.input-wrap').eq(2).css('display', 'flex');
   }
   if (name === 'embed') {
-    $('.btns').removeClass('extract').addClass(name)
-    $('.input-wrap').eq(0).css('display', 'flex')
+    $('.btns').removeClass('extract').addClass(name);
+    $('.input-wrap').eq(0).css('display', 'flex');
+    $('.input-wrap').eq(2).css('display', 'none');
   }
-})
+});
 
-$('.check-wrap').eq(0).click()
+$('.check-wrap').eq(0).click();
